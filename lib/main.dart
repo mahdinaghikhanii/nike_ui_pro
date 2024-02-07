f6bcf1d9a6e398e9d321d3d5d597798afba358d3
Mahdinkh7 <mahdinkh7@gmail.com>
2024-02-07 22:07:24 +0330

    create splash csrean

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nike/pages/home/home.dart';
+import 'package:nike/pages/splash/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,15 +8,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Nike',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const HomePages());
+        home: const SplashPage());
   }
 }
--- lib/pages/splash/splash.dart
+++ lib/pages/splash/splash.dart
@@ -7,9 +7,74 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          leading: Image.asset('assets/logo.png'),
+          backgroundColor: Colors.white,
+          centerTitle: false,
+          title: Padding(
+            padding: const EdgeInsets.only(left: 20, bottom: 20),
+            child: Image.asset(
+              'assets/logo.png',
+              width: 70,
+            ),
+          ),
+        ),
+        bottomNavigationBar: SafeArea(
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 60),
+            width: MediaQuery.sizeOf(context).width,
+            height: 54,
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                onPressed: () {},
+                child: const Center(
+                  child: Text(
+                    "Get Started",
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )),
+          ),
         ),
         backgroundColor: Colors.white,
-        body: Column(children: []));
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 20),
+                child: Image.asset(
+                  "assets/splash.png",
+                  fit: BoxFit.cover,
+                  height: 390,
+                  width: 390,
+                ),
+              ),
+              const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Text(
+                    "Summer\nCollections",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 40, height: 0),
+                  )),
+              const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Text(
+                    "2024",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                        color: Colors.grey),
+                  )),
+              const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 4),
+                  child: Text(
+                    "His ability to control his movement keeps defenders guessing",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.grey),
+                  )),
+            ]));
   }
 }
